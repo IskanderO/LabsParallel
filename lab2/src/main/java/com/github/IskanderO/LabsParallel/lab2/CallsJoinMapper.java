4016ee4ff8d0a4801b635d8c4811faacb672dcf7
IskanderO <kinder_008@mail.ru>
2020-12-02 21:31:13 +0600

    Scripted auto-commit on change (2020-12-02 21:31:13) by gitwatch.sh

--- lab2/src/main/java/com/github/IskanderO/LabsParallel/lab2/CallsJoinMapper.java
+++ lab2/src/main/java/com/github/IskanderO/LabsParallel/lab2/CallsJoinMapper.java
@@ -1,5 +1,17 @@
 package com.github.IskanderO.LabsParallel.lab2;
 
-public class CallsJoinMapper {
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+
+import java.io.IOException;
+
+public class CallsJoinMapper extends Mapper<LongWritable, Text, TextPair, Text> {
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        ServiceCall call = new ServiceCall(value);
+        context.write(new TextPair(call.getSystemA().toString(), "1"), new Text(call.toString())); // key, value
+    }
 
 }
