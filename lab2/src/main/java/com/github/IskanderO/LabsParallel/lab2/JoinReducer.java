da62cf3b9afeb4e3a209f1d9557c291ef46c2870
IskanderO <kinder_008@mail.ru>
2020-12-02 23:26:40 +0600

    Scripted auto-commit on change (2020-12-02 23:26:40) by gitwatch.sh

--- lab2/src/main/java/com/github/IskanderO/LabsParallel/lab2/JoinReducer.java
+++ lab2/src/main/java/com/github/IskanderO/LabsParallel/lab2/JoinReducer.java
@@ -4,7 +4,7 @@ import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
 
-public class JoinReducer extends Reducer {
+public class JoinReducer extends Reducer<TextPair, Text, Text, Text> {
 
     @Override
     protected void reduce(Object key, Iterable values, Context context) throws IOException, InterruptedException {
