b24f9828df1ae729ad007ad2688092433778f233
IskanderO <kinder_008@mail.ru>
2020-12-02 22:23:17 +0600

    Scripted auto-commit on change (2020-12-02 22:23:16) by gitwatch.sh

--- lab2/src/main/java/com/github/IskanderO/LabsParallel/lab2/FlightsJoinMapper.java
+++ lab2/src/main/java/com/github/IskanderO/LabsParallel/lab2/FlightsJoinMapper.java
@@ -18,7 +18,10 @@ public class FlightsJoinMapper extends Mapper<LongWritable, Text, TextPair, Text
         if (!key.equals(new LongWritable(0))) { // Пропускаем первую строку csv файла (наименования столбцов)
             String[] columns = value.toString().split(",");
 
-            
+            Integer destAirportId = Integer.parseInt(columns[DEST_AIRPORT_ID_COLUMN_NUMBER]);
+            String delay = columns[ARR_DELAY_NEW_COLUMN_NUMBER];
+
+            context.write(new TextPair(destAirportId, "1"), new Text(delay)); // key, value
         }
     }
 }
