e2bff0f21019c1ca325a934945d7c9e8e6020d42
IskanderO <kinder_008@mail.ru>
2020-12-02 22:23:01 +0600

    Scripted auto-commit on change (2020-12-02 22:23:01) by gitwatch.sh

--- lab2/src/main/java/com/github/IskanderO/LabsParallel/lab2/FlightsJoinMapper.java
+++ lab2/src/main/java/com/github/IskanderO/LabsParallel/lab2/FlightsJoinMapper.java
@@ -12,7 +12,7 @@ public class FlightsJoinMapper extends Mapper<LongWritable, Text, TextPair, Text
     private static final int DEST_AIRPORT_ID_COLUMN_NUMBER = 14; // DEST_AIRPORT_ID — Идентификатор аэропорта
 
     private static final int ARR_DELAY_NEW_COLUMN_NUMBER = 18; // ARR_DELAY_NEW - разница в минутах между расчетным временем приземления и реальным (>=0)
-    
+
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         if (!key.equals(new LongWritable(0))) { // Пропускаем первую строку csv файла (наименования столбцов)
