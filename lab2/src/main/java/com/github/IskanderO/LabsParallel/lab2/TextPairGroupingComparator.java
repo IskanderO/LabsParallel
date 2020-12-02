68f0af29239a8f83bfea36df3a3e5febf05f8ebd
IskanderO <kinder_008@mail.ru>
2020-12-02 23:15:24 +0600

    Scripted auto-commit on change (2020-12-02 23:15:24) by gitwatch.sh

--- lab2/src/main/java/com/github/IskanderO/LabsParallel/lab2/TextPairGroupingComparator.java
+++ lab2/src/main/java/com/github/IskanderO/LabsParallel/lab2/TextPairGroupingComparator.java
@@ -1,6 +1,17 @@
 package com.github.IskanderO.LabsParallel.lab2;
 
-public class TextPairGroupingComparator {
+import org.apache.hadoop.io.RawComparator;
+
+public class TextPairGroupingComparator implements RawComparator {
+    @Override
+    public int compare(byte[] bytes, int i, int i1, byte[] bytes1, int i2, int i3) {
+        return 0;
+    }
+
+    @Override
+    public int compare(Object o1, Object o2) {
+        return 0;
+    }
 
     
 }
