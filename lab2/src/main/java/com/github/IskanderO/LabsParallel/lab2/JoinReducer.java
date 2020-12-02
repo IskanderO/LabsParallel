3b4ac9f323c4a1bfa9456deefa7232af9a61bec2
IskanderO <kinder_008@mail.ru>
2020-12-02 23:27:11 +0600

    Scripted auto-commit on change (2020-12-02 23:27:11) by gitwatch.sh

--- lab2/src/main/java/com/github/IskanderO/LabsParallel/lab2/JoinReducer.java
+++ lab2/src/main/java/com/github/IskanderO/LabsParallel/lab2/JoinReducer.java
@@ -7,4 +7,8 @@ import java.io.IOException;
 
 public class JoinReducer extends Reducer<TextPair, Text, Text, Text> {
 
+    @Override
+    protected void reduce(TextPair key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        super.reduce(key, values, context);
+    }
 }
