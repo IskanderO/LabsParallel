ca0c611b870784c444166afbb426359c1994c3ac
IskanderO <kinder_008@mail.ru>
2020-12-02 22:21:51 +0600

    Scripted auto-commit on change (2020-12-02 22:21:51) by gitwatch.sh

--- lab2/src/main/java/com/github/IskanderO/LabsParallel/lab2/AirportsJoinMapper.java
+++ lab2/src/main/java/com/github/IskanderO/LabsParallel/lab2/AirportsJoinMapper.java
@@ -10,7 +10,9 @@ import java.io.IOException;
 public class AirportsJoinMapper extends Mapper<LongWritable, Text, TextPair, Text> {
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        SystemInfo system = new SystemInfo(value);
-        context.write(new TextPair(system.getSystemCode().toString(), "0"), new Text(system.toString()));  // key, value
+        if (!key.equals(new LongWritable(0))) { // Пропускаем первую строку csv файла (наименования столбцов)
+            SystemInfo system = new SystemInfo(value);
+            context.write(new TextPair(system.getSystemCode().toString(), "0"), new Text(system.toString()));  // key, value
+        }
     }
 }
