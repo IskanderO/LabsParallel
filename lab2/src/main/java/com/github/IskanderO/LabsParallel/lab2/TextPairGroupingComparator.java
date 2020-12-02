42e821569aa8913ab06d8d16388ec31ed565874e
IskanderO <kinder_008@mail.ru>
2020-12-02 23:11:01 +0600

    Scripted auto-commit on change (2020-12-02 23:11:01) by gitwatch.sh

--- lab2/src/main/java/com/github/IskanderO/LabsParallel/lab2/TextPairGroupingComparator.java
+++ lab2/src/main/java/com/github/IskanderO/LabsParallel/lab2/TextPairGroupingComparator.java
@@ -1,2 +1,4 @@
-package com.github.IskanderO.LabsParallel.lab2;public class TextPairGroupingComparator {
+package com.github.IskanderO.LabsParallel.lab2;
+
+public class TextPairGroupingComparator {
 }
