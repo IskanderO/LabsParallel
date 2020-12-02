963003f94c79d7d6343259dbe3a85ab2c60b40c9
IskanderO <kinder_008@mail.ru>
2020-12-02 22:24:57 +0600

    Scripted auto-commit on change (2020-12-02 22:24:57) by gitwatch.sh

--- lab2/src/main/java/com/github/IskanderO/LabsParallel/lab2/AirportsJoinMapper.java
+++ lab2/src/main/java/com/github/IskanderO/LabsParallel/lab2/AirportsJoinMapper.java
@@ -8,6 +8,12 @@ import org.apache.hadoop.io.Text;
 import java.io.IOException;
 
 public class AirportsJoinMapper extends Mapper<LongWritable, Text, TextPair, Text> {
+
+    //т.к. отсчет с нуля, то индексы на единицу меньше, чем в самом csv файле
+    private static final int DEST_AIRPORT_ID_COLUMN_NUMBER = 0; // DEST_AIRPORT_ID — Идентификатор аэропорта
+
+    private static final int AIRPORT_DESCRIPTION_COLUMN_NUMBER = 1; // DEST_AIRPORT_ID — Идентификатор аэропорта
+
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         if (!key.equals(new LongWritable(0))) { // Пропускаем первую строку csv файла (наименования столбцов)
