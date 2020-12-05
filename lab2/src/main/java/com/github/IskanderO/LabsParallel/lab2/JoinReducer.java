a48e04da5d815104b25aea80de498379e9a69cca
IskanderO <kinder_008@mail.ru>
2020-12-05 16:06:18 +0600

    Scripted auto-commit on change (2020-12-05 16:06:17) by gitwatch.sh

--- lab2/src/main/java/com/github/IskanderO/LabsParallel/lab2/JoinReducer.java
+++ lab2/src/main/java/com/github/IskanderO/LabsParallel/lab2/JoinReducer.java
@@ -25,8 +25,25 @@ class Statistics {
 
     private double averageDelay = 0.0;
 
+    public double getMinDelay() {
+        return minDelay;
+    }
 
+    public double getMaxDelay() {
+        return maxDelay;
+    }
 
+    public double getSumOfDelays() {
+        return sumOfDelays;
+    }
+
+    public int getCountOfDelays() {
+        return countOfDelays;
+    }
+
+    public double getAverageDelay() {
+        return averageDelay;
+    }
 
     public Statistics() {
     }
