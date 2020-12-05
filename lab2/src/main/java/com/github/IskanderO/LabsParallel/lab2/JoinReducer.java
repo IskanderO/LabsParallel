ee10cfec5ef467f48aea3b89b0438c9cb01f161f
IskanderO <kinder_008@mail.ru>
2020-12-05 16:27:53 +0600

    Scripted auto-commit on change (2020-12-05 16:27:53) by gitwatch.sh

--- lab2/src/main/java/com/github/IskanderO/LabsParallel/lab2/JoinReducer.java
+++ lab2/src/main/java/com/github/IskanderO/LabsParallel/lab2/JoinReducer.java
@@ -4,6 +4,7 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 public class JoinReducer extends Reducer<TextPair, Text, Text, Text> {
 
@@ -11,7 +12,7 @@ public class JoinReducer extends Reducer<TextPair, Text, Text, Text> {
     protected void reduce(TextPair key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
         Iterator<Text> iter = values.iterator();
         String airportDescription = iter.next().toString();
-        
+
     }
 }
 
