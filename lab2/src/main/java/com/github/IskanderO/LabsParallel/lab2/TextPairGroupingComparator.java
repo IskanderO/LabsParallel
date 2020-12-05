def9d8ffac3cec64c1316a25e336ff9c7815872f
IskanderO <kinder_008@mail.ru>
2020-12-05 16:42:09 +0600

    Scripted auto-commit on change (2020-12-05 16:42:09) by gitwatch.sh

--- lab2/src/main/java/com/github/IskanderO/LabsParallel/lab2/TextPairGroupingComparator.java
+++ lab2/src/main/java/com/github/IskanderO/LabsParallel/lab2/TextPairGroupingComparator.java
@@ -8,7 +8,7 @@ public class TextPairGroupingComparator extends WritableComparator {
         super(TextPair.class, true);
     }
 
-    public int compare(Object obj1, Object obj2) {
+    public int compare(WritableComparable obj1, WritableComparable obj2) {
         TextPair textPair1 = (TextPair)obj1;
         TextPair textPair2 = (TextPair)obj2;
         return Integer.compare(textPair1.getDestAirportId(), textPair2.getDestAirportId());
