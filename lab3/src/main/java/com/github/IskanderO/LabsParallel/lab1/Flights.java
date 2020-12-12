7e86afa0def368e132f02595f2d11fd89dc32b6e
IskanderO <kinder_008@mail.ru>
2020-12-12 20:54:02 +0600

    Scripted auto-commit on change (2020-12-12 20:54:02) by gitwatch.sh

--- lab3/src/main/java/com/github/IskanderO/LabsParallel/lab1/Flights.java
+++ lab3/src/main/java/com/github/IskanderO/LabsParallel/lab1/Flights.java
@@ -3,12 +3,15 @@ package com.github.IskanderO.LabsParallel.lab1;
 import java.io.Serializable;
 
 public class Flights implements Serializable {
+    private Double delay = 0.0;
+    
+
 
     public Flights(String delay, String cancelled) {
 
     }
 
     public Flights update(Flights newFlights) {
-        
+
     }
 }
