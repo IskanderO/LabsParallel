ab7db6498eff7021588db950d8e5305538e52a54
IskanderO <kinder_008@mail.ru>
2020-12-12 20:10:37 +0600

    Scripted auto-commit on change (2020-12-12 20:10:37) by gitwatch.sh

--- lab3/src/main/java/com/github/IskanderO/LabsParallel/lab1/Main.java
+++ lab3/src/main/java/com/github/IskanderO/LabsParallel/lab1/Main.java
@@ -7,11 +7,17 @@ import org.apache.spark.api.java.JavaSparkContext;
 public class Main {
 
     private static final String FILE_NAME_1 = "airports.csv";
+    private static final String FILE_NAME_2 = "flights.csv";
+
+
 
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
         JavaRDD<String> airportsDescriptionDataRDD = sc.textFile(FILE_NAME_1);
+        JavaRDD<String> flightsDataRDD = sc.textFile(FILE_NAME_2);
+
+        
     }
 }
