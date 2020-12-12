76323b8d2cf8f0896a37ed8cfceba4ca59f69f36
IskanderO <kinder_008@mail.ru>
2020-12-12 20:06:25 +0600

    Scripted auto-commit on change (2020-12-12 20:06:25) by gitwatch.sh

--- lab3/src/main/java/com/github/IskanderO/LabsParallel/lab1/Main.java
+++ lab3/src/main/java/com/github/IskanderO/LabsParallel/lab1/Main.java
@@ -2,6 +2,7 @@ package com.github.IskanderO.LabsParallel.lab1;
 
 public class Main {
     public static void main(String[] args) {
-        
+        SparkConf conf = new SparkConf().setAppName("lab3");
+        JavaSparkContext sc = new JavaSparkContext(conf);
     }
 }
