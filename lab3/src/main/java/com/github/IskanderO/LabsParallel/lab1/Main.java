0562a545913989345a36d187ecbb600645e36a7e
IskanderO <kinder_008@mail.ru>
2020-12-14 19:16:31 +0600

    Scripted auto-commit on change (2020-12-14 19:16:31) by gitwatch.sh

--- lab3/src/main/java/com/github/IskanderO/LabsParallel/lab1/Main.java
+++ lab3/src/main/java/com/github/IskanderO/LabsParallel/lab1/Main.java
@@ -59,7 +59,7 @@ public class Main {
                     String airportName = broadcast.value().get(e._1._1);
                     String destName = broadcast.value().get(e._1._2);
                     String name = airportName + " ===> " + destName;
-                    String stat = " flights: " + e._2.getFlightsCount() + " max delay: " + e._2.getDelay() + " ratio: " + e._2.getCounter();
+                    String stat = " flights: " + e._2.getFlightsCount() + " max delay: " + e._2.getDelay() + " ratio: " + (double) e._2.getCounter() / (double)e._2.getFlightsCount();
                     return new Tuple2<>(name, stat);
                 }
         );
