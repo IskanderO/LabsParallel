1af2434e75ed2786d39c6aec81bb0211bf709277
IskanderO <kinder_008@mail.ru>
2020-12-14 16:38:45 +0600

    Scripted auto-commit on change (2020-12-14 16:38:45) by gitwatch.sh

--- lab3/src/main/java/com/github/IskanderO/LabsParallel/lab1/Flights.java
+++ lab3/src/main/java/com/github/IskanderO/LabsParallel/lab1/Flights.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 public class Flights implements Serializable {
     private Double delay = 0.0;
     private Boolean cancelled = false;
-
+    private Integer counter = 0;
 
     public Flights(String delay, String cancelled) {
         if (!delay.isEmpty())
@@ -29,7 +29,7 @@ public class Flights implements Serializable {
             delay = newDelay;
         }
         if (newFlights.getCancelled() || newDelay > 0.00001) {
-            
+
         }
     }
 }
