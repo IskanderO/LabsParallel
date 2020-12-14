783dc2e9f1ece331a309b824fd95fe08ff854038
IskanderO <kinder_008@mail.ru>
2020-12-14 19:11:38 +0600

    Scripted auto-commit on change (2020-12-14 19:11:38) by gitwatch.sh

--- lab3/src/main/java/com/github/IskanderO/LabsParallel/lab1/Main.java
+++ lab3/src/main/java/com/github/IskanderO/LabsParallel/lab1/Main.java
@@ -58,7 +58,7 @@ public class Main {
                 e -> {
                     String airportName = broadcast.value().get(e._1._1);
                     String destName = broadcast.value().get(e._1._2);
-                    String name = airportName + ;
+                    String name = airportName + " ===> " + destName;
                     String stat = "";
                     return new Tuple2<>(name, stat);
                 }
